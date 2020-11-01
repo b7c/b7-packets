e3a0a847ce3bb99ed4603a6ce67caa4a7810725f
b7c <58299468+b7c@users.noreply.github.com>
2020-11-01 14:30:56 +1300

    Fixed null reference error that sometimes occurs when opening the module

--- b7-packets/MainWindow.xaml.cs
+++ b7-packets/MainWindow.xaml.cs
@@ -8,6 +8,8 @@ namespace b7.Packets
 {
     partial class MainWindow : ExtensionWindow
     {
+        private bool _initialized;
+
         public new PacketsModule Module => (PacketsModule)base.Module;
 
         public MainWindow()
@@ -20,10 +22,19 @@ namespace b7.Packets
         public override void OnAttach()
         {
             messagesView.LoadMessages(Module.Game);
+
+            _initialized = true;
         }
 
-        public override void HandleIncoming(DataInterceptedEventArgs e) => packetLogger.HandleData(e);
-        public override void HandleOutgoing(DataInterceptedEventArgs e) => packetLogger.HandleData(e);
+        public override void HandleIncoming(DataInterceptedEventArgs e) => HandleData(e);
+        public override void HandleOutgoing(DataInterceptedEventArgs e) => HandleData(e);
+
+        private void HandleData(DataInterceptedEventArgs e)
+        {
+            if (!_initialized) return;
+
+            packetLogger.HandleData(e);
+        }
 
         public void LoadInStructuralizer(VmPacketLog log)
         {
